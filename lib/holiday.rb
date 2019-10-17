f5ef95e02cff6f1db02524577000f2be62abf27d
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 13:35:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,8 +54,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     if seasons == season
     # we are inside of holidays = {:thanksgiving=>["Turkey"]}
       holidays[holiday_name]= supply_array
-    #
-      binding.pry
+    # we add a new pair with the variables provided by the test
     end
   end
 end
