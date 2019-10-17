c9e7352cd3d970b7510a767ee6992d3429d96cc5
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 15:19:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,11 +92,13 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_holidays = []
   holiday_hash.map do |seasons, holidays|
     holidays.map do |holiday, supplies|
-      holiday
+      bbq_holidays << holiday
     end
   end
+  bbq_holidays
 end
 
 
