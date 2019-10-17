92293a9f9be459edc407a4f989435b6896c14a08
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 14:13:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,6 +64,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.map do |seasons, holidays|
     if seasons == :winter
       holidays.map do |holiday, supplies| 
+        supplies
         return supplies
       end
     end
