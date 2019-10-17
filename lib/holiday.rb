4d4b79a9c3fbdf15085766759b7a78fd8f73eec3
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 13:49:42 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,9 @@ def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
   winter_supplies = []
   holiday_hash.each do |seasons, holidays|
-       holidays.each { |holiday, supplies| winter_supplies << supplies}
+       if seasons == :winter
+         holidays.each { |holiday, supplies| winter_supplies << supplies}
+      end
       # binding.pry
     #end
   winter_supplies
