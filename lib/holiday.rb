fc7897960bb5e05ffe6a22e08a61556d362d82ad
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 14:38:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,10 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |seasons, holidays|
     puts "#{seasons.to_s.capitalize}:"
       holidays.each do |holiday, supply|
-        puts "  #{holiday.to_s.capitalize}: #{supply.join(", ")}"
+        if holiday.split == false
+          puts "  #{holiday.to_s.capitalize}: #{supply.join(", ")}"
+        else
+          holiday
       end
   end
 
