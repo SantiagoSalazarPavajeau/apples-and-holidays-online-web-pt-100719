b3362a31990ef148df8c6c9605467b62b13a8569
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 14:26:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  holiday_hash.join(" , ")
+  holiday_hash.each do |seasons, holidays|
+    puts seasons.to_s.capitalize
+  end
 
 end
 
