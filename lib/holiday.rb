7b8fbc90f83bd088b7045350b04c3c3bab5eca5a
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 12:36:18 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -31,12 +31,10 @@ end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
   # holiday_hash[:spring][:memorial_day].push("Grill") || holiday_hash[:spring][:memorial_day].push("Table Cloth")
   holiday_hash.each do | season, holiday|
     if holiday == :memorial_day
-      holiday << supply
+      :memorial_day.push(supply)
     end
   end
 end
