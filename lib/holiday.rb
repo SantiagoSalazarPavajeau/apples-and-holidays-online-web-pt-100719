c1d31962c3ff0d2c9de4e89c3898a6918dc66638
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-17 13:12:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,7 +29,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash[:winter][:new_years][-1]=supply
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday << supply
+      binding.pry
+      holiday.push(supply)
     end
   end
 end
